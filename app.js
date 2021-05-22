2be3192def3c70b40b35de1dd4c690622333d45f
Sebastian Jooss <sebastian.jooss@googlemail.com>
2021-05-22 04:38:34 +0200

    added new game functionality

--- app.js
+++ app.js
@@ -7,18 +7,41 @@ const app = Vue.createApp({
         return {
             monsterHealth: 100,
             playerHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         };
     },
     computed: {
         monsterBarStyles(){
+            if (this.monsterHealth < 0) {
+                return {width: "0%"};
+            }
             return {width: this.monsterHealth + "%"};
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return {width: "0%"};
+            }
             return {width: this.playerHealth + "%"};
         },
         mayUseSpecialAttack() {
             return this.currentRound % 3 !== 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0 ) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'drawr';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
         }
     },
     methods: {
@@ -46,6 +69,12 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
         }
     },
 });
